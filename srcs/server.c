bcae9ea6e12f9de55894ccddd4810d1e8e5af03d
nfauconn <nfauconn@student.42.fr>
2021-09-04 17:06:43 +0200

    ddd

--- srcs/server.c
+++ srcs/server.c
@@ -6,7 +6,7 @@
 /*   By: nfauconn <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 17:09:29 by nfauconn          #+#    #+#             */
-/*   Updated: 2021/09/04 16:26:18 by nfauconn         ###   ########.fr       */
+/*   Updated: 2021/09/04 17:06:37 by nfauconn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	main(int argc, char **argv)
 	(void)argc;
 	ft_printf("%d\n", getpid());
 	s.sa_handler = signal_handler(sig_num);
-	sigaction(sig_num, &s, NULL);
+	sigaction(SIGUSR1, &s, NULL);
 	while (1)
 	{
 		pause();
